03ccce487bacdde11757a958942141332c202226
Marc Catrisse <marc@catrisse.net>
2021-05-18 19:30:02 +0200

    grasp works

--- Heuristics/solutions/example.sol
+++ Heuristics/solutions/example.sol
@@ -1,5 +1,9 @@
-z = 1.00000000;
-Talk 4 --> slot 1, room 1
-Talk 3 --> slot 2, room 1
-Talk 2 --> slot 1, room 2
-Talk 1 --> slot 2, room 2
+z = 0.00000000;
+Talk 2 --> slot 4, room 3
+Talk 5 --> slot 1, room 2
+Talk 4 --> slot 4, room 1
+Talk 7 --> slot 2, room 1
+Talk 3 --> slot 1, room 1
+Talk 11 --> slot 3, room 3
+Talk 10 --> slot 3, room 1
+Talk 9 --> slot 2, room 2
--- Heuristics/solvers/solver_GRASP.py
+++ Heuristics/solvers/solver_GRASP.py
@@ -29,17 +29,17 @@ class Solver_GRASP(_Solver):
     def _selectCandidate(self, candidateList, alpha):
 
         # sort candidate assignments by highestLoad in ascending order
-        sortedCandidateList = sorted(candidateList, key=lambda x: x.highestLoad)
+        sortedCandidateList = sorted(candidateList, key=lambda x: x.cost)
         
         # compute boundary highest load as a function of the minimum and maximum highest loads and the alpha parameter
-        minHLoad = sortedCandidateList[0].highestLoad
-        maxHLoad = sortedCandidateList[-1].highestLoad
-        boundaryHLoad = minHLoad + (maxHLoad - minHLoad) * alpha
+        minHCost = sortedCandidateList[0].cost
+        maxHCost = sortedCandidateList[-1].cost
+        boundaryHCost = minHCost + (maxHCost - minHCost) * alpha
         
         # find elements that fall into the RCL
         maxIndex = 0
         for candidate in sortedCandidateList:
-            if candidate.highestLoad <= boundaryHLoad:
+            if candidate.cost <= boundaryHCost:
                 maxIndex += 1
 
         # create RCL and pick an element randomly
@@ -52,16 +52,14 @@ class Solver_GRASP(_Solver):
         solution = self.instance.createSolution()
         
         # get tasks and sort them by their total required resources in descending order
-        tasks = self.instance.getTasks()
-        sortedTasks = sorted(tasks, key=lambda t: t.getTotalResources(), reverse=True)
-
+        talks = range(0, self.instance.getN())
+        sortedTalks = sorted(talks, key=lambda t: (self.instance.computePPair(t), self.instance.computeSPair(t)), reverse=True)
 
         # for each task taken in sorted order
-        for task in sortedTasks:
-            taskId = task.getId()
+        for talk in sortedTalks:
             
             # compute feasible assignments
-            candidateList = solution.findFeasibleAssignments(taskId)
+            candidateList = solution.findFeasibleAssignments(talk)
 
             # no candidate assignments => no feasible assignment found
             if not candidateList:
@@ -72,7 +70,7 @@ class Solver_GRASP(_Solver):
             candidate = self._selectCandidate(candidateList, alpha)
 
             # assign the current task to the CPU that resulted in a minimum highest load
-            solution.assign(taskId, candidate.cpuId)
+            solution.assign(talk, candidate.time_slot, candidate.room)
             
         return solution
     
@@ -84,8 +82,8 @@ class Solver_GRASP(_Solver):
         self.startTimeMeasure()
         incumbent = self.instance.createSolution()
         incumbent.makeInfeasible()
-        bestHighestLoad = incumbent.getCost()
-        self.writeLogLine(bestHighestLoad, 0)
+        bestHighestCost = incumbent.getCost()
+        self.writeLogLine(bestHighestCost, 0)
 
         iteration = 0
         while not self.stopCriteria():
@@ -101,13 +99,13 @@ class Solver_GRASP(_Solver):
                 solution = localSearch.solve(solution=solution, startTime=self.startTime, endTime=endTime)
 
             if solution.isFeasible():
-                solutionHighestLoad = solution.getCost()
-                if solutionHighestLoad < bestHighestLoad :
+                solutionHighestCost = solution.getCost()
+                if solutionHighestCost < bestHighestCost:
                     incumbent = solution
-                    bestHighestLoad = solutionHighestLoad
-                    self.writeLogLine(bestHighestLoad, iteration)
+                    bestHighestCost = solutionHighestCost
+                    self.writeLogLine(bestHighestCost, iteration)
 
-        self.writeLogLine(bestHighestLoad, iteration)
+        self.writeLogLine(bestHighestCost, iteration)
         self.numSolutionsConstructed = iteration
         self.printPerformance()
         return incumbent
